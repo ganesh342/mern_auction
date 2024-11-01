a18e4205f66996323535eba35133ff4734b2146f
ganesh342 <131640304+ganesh342@users.noreply.github.com>
2024-11-02 01:28:47 +0530

    first commit

---
+++ backend/database/connection.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+export const connection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN_AUCTION_PLATFORM",
+    })
+    .then(() => {
+      console.log("Connected to database.");
+    })
+    .catch((err) => {
+      console.log(`Some error occured while connecting to database: ${err}`);
+    });
+};
